f305536d2d2cc9733acd2a39dad5f675b67050cb
IsCathal <mccabemcathal@gmail.com>
2024-07-22 15:27:28 +0100

    adding breed

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+const Breed = [];
 
 
 const SearchParams = () => {
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState(""); 
+  const [breed, setBreed] = useState("");
   return (
     <div className="search-params">
       <form>
@@ -20,7 +22,10 @@ const SearchParams = () => {
         <label htmlFor="animal">
           Animal
           <select
-            onChange={(e) => setAnimal(e.target.value)}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
             onBlur={(e) => setAnimal(e.target.value)}
             id="animal"
             value={animal}
@@ -29,6 +34,23 @@ const SearchParams = () => {
             {ANIMALS.map((animal) => (
               <option key={animal} value={animal}>
                 {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+            id="breed"
+            disabled={breed.length === 0}
+            value={breed}
+          >
+            <option />
+            {Breed.map((breed) => (
+              <option key={breed} value={breed}>
+                {breed}
               </option>
             ))}
           </select>
