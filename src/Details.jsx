c2c155cb28caf026be2fd0265a3e8ee64a7c54f5
IsCathal <mccabemcathal@gmail.com>
2024-07-24 12:01:55 +0100

    Refactored fetchBreedList

--- src/Details.jsx
+++ src/Details.jsx
@@ -5,6 +5,7 @@ import fetchPet from "./fetchPet";
 const Details = () => {
   const { id } = useParams();
   const results = useQuery(['details', id], fetchPet);
+  
 
   if (results.isLoading) {
     return (
